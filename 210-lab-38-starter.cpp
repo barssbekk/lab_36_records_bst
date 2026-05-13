548db41332494402463eaed3eee843e22cc05e50
Barsbek Azamatov <bars.moderside@gmail.com>
2026-05-13 13:02:31 -0700

    fix file input

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -1,10 +1,11 @@
 // COMSC-210 || Lab 38 || Barsbek
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "IntBinaryTree.h"
 using namespace std;
 
 int main() {
-
+    ifstream fileInput = "codes.txt";
     return 0;
 }
