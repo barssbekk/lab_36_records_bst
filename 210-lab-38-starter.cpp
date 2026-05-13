f43716d64e4d6c43ec5227ad6113c2f59f6859a7
Barsbek Azamatov <bars.moderside@gmail.com>
2026-05-13 13:07:20 -0700

    Milestone 3

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -6,6 +6,18 @@
 using namespace std;
 
 int main() {
-    ifstream fileInput = "codes.txt";
+    ifstream fileInput("codes.txt");
+    if (!fileInput) { // check if file opens
+        cerr << "File not found\n";
+        return 1;
+    }
+    IntBinaryTree tree;
+    string code;
+
+    while (fileInput >> code)
+        tree.insertNode(code);
+
+    tree.displayInOrder();
+
     return 0;
 }
