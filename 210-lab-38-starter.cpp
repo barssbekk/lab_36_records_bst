5bc840838886940c8af5ba4690502137d22810b5
Barsbek Azamatov <bars.moderside@gmail.com>
2026-05-13 13:14:11 -0700

    fix menu

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -5,6 +5,8 @@
 #include "IntBinaryTree.h"
 using namespace std;
 
+const int MAX_AMOUNT_CHOICE = 5;
+
 int main() {
     ifstream fileInput("codes.txt");
     if (!fileInput) { // check if file opens
@@ -17,7 +19,18 @@ int main() {
     while (fileInput >> code)
         tree.insertNode(code);
 
-    tree.displayInOrder();
+    int choice = 0;
+    do
+    {
+        cout << "\n---MENU---\n"
+             << "1. Add\n"
+             << "2. Delete\n"
+             << "3. Search\n"
+             << "4. Display\n"
+             << "5. Exit\n"
+             << "Enter choice: ";
+        cin >> choice;
+    } while (choice <= MAX_AMOUNT_CHOICE && choice > 0);
 
     return 0;
 }
